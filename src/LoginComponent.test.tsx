1b56a203abb0fb9831ebc8f5c9b83a4a23d95fb5
Ali salehi <parkour.runner@gmail.com>
2024-11-09 13:41:48 +0300

    Testing async updates

--- src/LoginComponent.test.tsx
+++ src/LoginComponent.test.tsx
@@ -43,16 +43,80 @@ describe("Login conmponent tests", () => {
     expect(inputs[2].value).toBe("Login");
   });
 
-  
-
-  it("Clivk login with incomplete credentials - show right message", () => {
+  it("Click login with incomplete credentials - show right message", () => {
     const inputs = screen.getAllByTestId("input");
     const loginButton = inputs[2];
 
     // fireEvent.click(loginButton);
-    act(() =>{user.click(loginButton)});
+    act(() => {
+      user.click(loginButton);
+    });
     const resultLAbel = screen.getByTestId("resultLabel");
-    expect(resultLAbel.textContent).toBe("UserName and password required!")
+    expect(resultLAbel.textContent).toBe("UserName and password required!");
   });
 
+  it("Click login with complete credentials - give key", async () => {
+    loginServiceMock.login.mockResolvedValueOnce("1234");
+
+    const inputs = screen.getAllByTestId("input");
+    const userNameInput = inputs[0];
+    const passwordInput = inputs[1];
+    const loginButton = inputs[2];
+
+    fireEvent.change(userNameInput, { target: { value: "user" } });
+    fireEvent.change(passwordInput, { target: { value: "passWrong" } });
+    fireEvent.click(loginButton);
+    expect(loginServiceMock.login).toHaveBeenCalledWith("user", "passWrong");
+
+    const resultLAbel = await screen.findByTestId("resultLabel");
+    expect(resultLAbel.textContent).toBe("successful login");
+  });
+
+  it("Click login with complete credentials - with user calls", async () => {
+    loginServiceMock.login.mockResolvedValueOnce("1234");
+
+    const inputs = screen.getAllByTestId("input");
+    const userNameInput = inputs[0];
+    const passwordInput = inputs[1];
+    const loginButton = inputs[2];
+
+    act(() => {
+      user.click(userNameInput);
+      user.keyboard("user");
+
+      user.click(passwordInput);
+      user.keyboard("passWrong");
+
+      user.click(loginButton);
+    });
+
+    expect(loginServiceMock.login).toHaveBeenCalledWith("user", "passWrong");
+
+    const resultLAbel = await screen.findByTestId("resultLabel");
+    expect(resultLAbel.textContent).toBe("successful login");
+  });
+
+  it("Click login - show error wrong creds", async () => {
+    loginServiceMock.login.mockResolvedValueOnce(undefined);
+
+    const inputs = screen.getAllByTestId("input");
+    const userNameInput = inputs[0];
+    const passwordInput = inputs[1];
+    const loginButton = inputs[2];
+
+    // act(() => {
+      user.click(userNameInput);
+      user.keyboard("user");
+
+      user.click(passwordInput);
+      user.keyboard("passWrong");
+
+      user.click(loginButton);
+    // });
+
+    expect(loginServiceMock.login).toHaveBeenCalledWith("user", "passWrong");
+
+    const resultLAbel = await screen.findByTestId("resultLabel");
+    expect(resultLAbel.textContent).toBe("invalid credentials");
+  });
 });
