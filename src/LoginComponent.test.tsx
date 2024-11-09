aa9efffbe21fbb7a6f76f3d8ff8ca8977ec6b9ca
Ali salehi <parkour.runner@gmail.com>
2024-11-09 13:19:57 +0300

    Query for elements

--- src/LoginComponent.test.tsx
+++ src/LoginComponent.test.tsx
@@ -7,16 +7,39 @@ describe("Login conmponent tests", () => {
   };
   const setTokenMock = jest.fn();
 
-  it("Should render conponent correctly", () => {
-    const { container } = render(
+  let container: HTMLElement;
+
+  function setup() {
+    container = render(
       <LoginComponent loginService={loginServiceMock} setToken={setTokenMock} />
-    );
+    ).container;
+  }
 
+  beforeEach(() => {
+    setup();
+  });
 
-
-    console.log(container.innerHTML);
-
+  it("Should render conponent correctly", () => {
     const mainElement = screen.getByRole("main");
     expect(mainElement).toBeInTheDocument();
+    expect(screen.queryByTestId("resultLabel")).not.toBeInTheDocument();
   });
+
+  it("Should render correctly - query by testid", () => {
+    const inputs = screen.getAllByTestId("input");
+    expect(inputs).toHaveLength(3);
+    expect(inputs[0].getAttribute("value")).toBe("");
+    expect(inputs[1].getAttribute("value")).toBe("");
+    expect(inputs[2].getAttribute("value")).toBe("Login");
+  });
+
+  it("Should render correctly - query by document queries", () => {
+    // eslint-disable-next-line testing-library/no-node-access
+    const inputs = document.querySelectorAll("input");
+    expect(inputs).toHaveLength(3);
+    expect(inputs[0].value).toBe("");
+    expect(inputs[1].value).toBe("");
+    expect(inputs[2].value).toBe("Login");
+  });
+
 });
